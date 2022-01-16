90d79091b2a096c06a1ca7941a638d01285bb559
mgarciap <manu230292@outlook.es>
2022-01-16 15:14:15 +0000

    commit .net api

---
+++ .NET.API/API.NET/API.NET/Controllers/HelloController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.NET.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Get()
+        {
+            return Ok(new { hello = "World" });
+        }
+    }
+}
